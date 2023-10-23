3762db5de150f51f765efef975ec285396570da5
Gregor Volberg <66525570+GregorVolberg@users.noreply.github.com>
2023-10-23 21:49:59 +0200

    new

--- Sitzung_02.R
+++ Sitzung_02.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 library(datasets)
+library(patchwork)
+library(ggh4x)
 
 ggplot(data = mpg, 
        mapping = aes(x = displ,
@@ -111,11 +113,76 @@ ggplot(dat, mapping = aes(a, b, color = grp)) +
    geom_point() +
    stat_smooth(dat, mapping = aes(a, b, group = grp), method = lm, se = 1) +
    scale_colour_manual(values=c("red", "blue")) +
-   theme(legend.position = "none") +
-   annotate("text", x=1,y=2, label = 'Group 1', color = 'red') +
-   annotate("text", x=-1,y=2, label = 'Group 2', color = 'blue')
+   annotate("text", x=1.5, y=2, label = 'Group 1', color = 'red', 
+            vjust = "middle", hjust = 'right') +
+   annotate("text", x=-1, y=2, label = 'Group 2', color = 'blue',
+            vjust = "middle", hjust = 'left') + 
+   scale_x_continuous(limits = c(-3, 3),
+                     breaks = seq(-3,3,1),
+                     minor_breaks = seq(-3,3,0.5),
+                     name = "Variable A",
+                     guide = "axis_minor") + 
+   scale_y_continuous(limits = c(-3, 3),
+                     breaks = seq(-3,3,1),
+                     minor_breaks = seq(-3,3,0.5),
+                     name = "Variable B", 
+                     guide = "axis_minor") +
+  theme_bw() +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    legend.position = "none",
+    axis.ticks.length = unit(0.2, "cm"),
+    ggh4x.axis.ticks.length.minor = rel(0.5))
 
+# pseudo data
+erp = tibble(wave1 = sin(seq(-pi,pi,0.1)),
+             wave2 = sin(seq(pi,-pi,-0.1)),
+             timebin = seq(1,length(wave1),1))
+timeBinMs = 40
+offset    = 21
+
+ggplot() +
+  geom_line(erp, mapping = aes((timebin-offset)*timeBinMs, wave2),
+            color = 'blue') +
+  geom_line(erp, mapping = aes((timebin-offset)*timeBinMs, wave1),
+            color = 'red') +
+  scale_x_continuous(breaks = c(-800,-400, 400, 800, 1200, 1600),
+                     name = "Time (ms)") + 
+  scale_y_continuous(limits = c(-1, 1),
+                     breaks = seq(-0.5,0.5),
+                     name = "microVolt") +
+  theme_bw() +
+  theme(
+    axis.line = element_line(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    legend.position = "none",
+    axis.ticks.length = unit(0.2, "cm"),
+    axis.text  = element_text(size=12),
+    axis.title = element_text(size=12),
+    panel.border = element_blank()) +
+    coord_axes_inside(xintercept = offset, yintercept=0, labels_inside = TRUE) +
+    guides(x = guide_axis_truncated(trunc_lower = -800,
+                       trunc_upper = 1600),
+           y = guide_axis_truncated(trunc_lower = -0.5,
+                                     trunc_upper = 0.5)) +
+    annotate("rect", xmin = 300, xmax = 400, ymin = -0.5, ymax = 0.5,
+           alpha = .1,fill = "blue") +
+  annotate("text", x=-400, y=-0.5, label = 'Wave 1', color = 'red', 
+           vjust = "middle", hjust = 'center') +
+  annotate("text", x=-400, y=0.5, label = 'Wave 2', color = 'blue',
+           vjust = "middle", hjust = 'center')
   
+  
+
+
+
+ggplot(erp, aes((timebin-offset)*timeBinMs, wave)) +
+  geom_line() +
+  scale_x_continuous(trans = (erp$timebin - 20)*40)
+                     
+  expand = c(0, 0)) 
 
 g1 <- dat %>% filter(grp == 1)
 g2 <- dat %>% filter(grp == 2)
@@ -129,7 +196,9 @@ library(patchwork)
   
 plt1 <- ggplot() +
   geom_point(g1, mapping = aes(a, b), color = 'blue') +
-  stat_smooth(g1, mapping = aes(a, b),method = lm, se = 1, color = 'blue')
+  stat_smooth(g1, mapping = aes(a, b),method = lm, se = 1, color = 'blue') +
+  labs(tag = "A")
+
 
 plt1 | plt1 / plt1
 
