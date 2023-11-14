a2ccee190605a90198982e2864798801cfdaa0f2
Gregor Volberg <gregor.volberg@ur.de>
2023-11-14 08:02:26 +0100

    facets

--- GruppeB.R
+++ GruppeB.R
@@ -79,9 +79,30 @@ plot1 | plot2
 plot1 | plot2 / plot1
 
 
-# # faceting
-# dframe2 <- tibble(
-#   av = rnorm(200, 10, 1),
-#   uvA = as.factor(rep(c("A1", "A2"), each = 100)),
-#   uvB = as.factor(rep(rep(c("B1", "B2"), each = 50), 2)),
-#   uvC = as.factor(rep(rep(c("C1", "C2"), each = 25), 4)))
+# faceting
+dframe2 <- tibble(
+  av = rnorm(200, 10, 1),
+  uvA = as.factor(rep(c("A1", "A2"), each = 100)),
+  uvB = as.factor(rep(rep(c("B1", "B2"), each = 50), 2)),
+  uvC = as.factor(rep(rep(c("C1", "C2"), each = 25), 4)))
+
+ggplot(dframe2, aes(x = uvA, y = av)) +
+  geom_jitter(aes(color = uvB),
+              size = 2,
+              position = position_jitterdodge(
+                jitter.width = 0.2,
+                dodge.width  = 0.6)) +
+  stat_summary(
+    aes(group = uvB),
+    fun.data="mean_se",
+    fun.args = list(mult = 1.5), 
+    size = 0.6,
+    linewidth = 1,
+    position = position_dodge(0.6)) +
+  facet_wrap(~uvC, strip.position = "bottom") +
+  theme_classic() + 
+  theme(strip.background = element_blank())
+
+# labelling siehe 
+# https://stackoverflow.com/questions/11889625/annotating-text-on-individual-facet-in-ggplot2
+
